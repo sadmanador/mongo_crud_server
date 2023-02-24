7644c4cad05d671b30e2e71074c100afba64a173
sadmanador <sadman.ador@gmail.com>
2023-02-24 16:07:03 +0600

    65-5 done
    get method working

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 //middleware
 app.use(cors());
@@ -10,31 +10,42 @@ app.use(express.json());
 
 //mongo connection
 
-const uri = "mongodb+srv://user02:WDDnKhnB4PHMXwdC@cluster0.9mathic.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-async function run(){
-  try{
-    const userCollection = client.db('simple-node-mongo-server').collection('mongo_crud_server');
-    //post user data from inputField
-    app.post('/users',async(req,res)=>{
+const uri =
+  "mongodb+srv://user02:WDDnKhnB4PHMXwdC@cluster0.9mathic.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    const userCollection = client
+      .db("simple-node-mongo-server")
+      .collection("mongo_crud_server");
+    
+      //post user data from inputField
+    app.post("/users", async (req, res) => {
       const user = req.body;
-      const result = await userCollection.insertOne(user)
-      res.send(result)
-    })
-  }
-  finally{
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
 
+    //read method
+    app.get('/users',async(req,res)=>{
+      const query = {}
+      const cursor = userCollection.find(query);
+      const result = await cursor.toArray()
+      res.send(result);
+    })
+  } finally {
   }
 }
-run().catch(console.dir)
+run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-res.send('Hello world')
+app.get("/", (req, res) => {
+  res.send("Hello world");
 });
 
 app.listen(port, () => {
-console.log(`listening port 5000`);
+  console.log(`listening port 5000`);
 });
